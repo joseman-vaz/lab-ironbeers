93d15bafbfae644c966dbc0ce7a55b30fa9772ee
Jose Vazquez <vazquezjoseman13@gmail.com>
2023-04-04 20:53:14 +0200

    Done no Bonus

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
-
+hbs.registerPartials(__dirname + '/views/partials');
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
@@ -18,8 +18,23 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Add the route handlers here:
 
-app.get('/', (req, res) => {
-  res.render('index');
+app.get('/', (req, res, next) => res.render('index'));
+app.get('/beers', (req, res, next) => {
+  // const beers = [{ name: 'kro' }, { name: '8.6' }];
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => {
+      res.render('beers', { beers: beersFromApi });
+    })
+    .catch(error => console.log(error));
+});
+app.get('/randomBeer', async (req, res, next) => {
+  try {
+    const randomBeer = await punkAPI.getRandom();
+    res.render('randomBeer', { randomBeer });
+  } catch (error) {
+    next(error);
+  }
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
